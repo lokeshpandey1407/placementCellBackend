3b6d13936883099f3478fe59281fc0f0d96d3906
lokesh pandey <luvpnd@gmail.com>
2024-06-28 10:57:24 +0530

    first commit

---
+++ src/features/student/student.controller.js
@@ -0,0 +1,40 @@
+import ApplicationError from "../../middleware/handleError.middleware.js";
+import StudentRepository from "./student.repository.js";
+
+export default class StudentController {
+  constructor() {
+    this.studentRepository = new StudentRepository();
+  }
+  async create(req, res, next) {
+    const studentData = req.body;
+    studentData.createdAt = new Date().toLocaleString();
+    try {
+      const student = await this.studentRepository.create(studentData);
+      if (!student) {
+        throw new ApplicationError("Cannot create Student", 500);
+      } else {
+        res.status(201).json({
+          success: true,
+          message: "Successfully created student record",
+          data: student,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const student = await this.studentRepository.getAll();
+      res.status(201).json({
+        success: true,
+        message: "",
+        data: student,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ src/features/student/student.schema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const StudentSchema = new mongoose.Schema(
+  {
+    batch: { type: String, required: true },
+    name: { type: String, required: true },
+    mobile: { type: Number },
+    email: { type: String },
+    college: { type: String },
+    jobStatus: { type: String, enum: ["Placed", "Not-placed"] },
+    DSAScore: { type: Number, require: true },
+    WebDScore: { type: Number, require: true },
+    reactScore: { type: Number, require: true },
+    interviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Interview",
+      },
+    ],
+    results: [
+      { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Result" },
+    ],
+    createdAt: { type: String },
+  },
+  { timeStamp: true }
+);
+
+StudentSchema.pre("save", function () {
+  this.interviews = [];
+  this.results = [];
+});
+
+const StudentModel = mongoose.model("Student", StudentSchema);
+export default StudentModel;
